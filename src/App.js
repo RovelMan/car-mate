1b6119f8d5dbd581d428beb7310defe0f5d492d1
RovelMan <jotdeepgahla@hotmail.com>
2021-01-02 16:02:55 +0100

    Add routing, navbar and pages

--- src/App.js
+++ src/App.js
@@ -1,34 +1,116 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import './App.css';
-import { Button } from 'antd';
+import Navbar from "./components/Navbar/Navbar";
+import { Layout, Menu } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 
-function App() {
+const { SubMenu } = Menu;
+const { Content, Sider } = Layout;
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>
-          <Button type="primary">Primary Button</Button>
-          <Button>Default Button</Button>
-          <Button type="dashed">Dashed Button</Button>
-          <br />
-          <Button type="text">Text Button</Button>
-          <Button type="link">Link Button</Button>
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Navbar />
+        <Layout>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </Layout>
+      </Layout>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return (
+    <Layout style={{ padding: '0 24px 24px' }}>
+      <Content
+        className="site-layout-background"
+        style={{
+          padding: 24,
+          marginTop: 24,
+          minHeight: 280,
+        }}
+      >
+        Home
+      </Content>
+    </Layout>
+  );
+}
+
+function Dashboard() {
+  return (
+    <Layout>
+      <Sider width={200} className="site-layout-background">
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          style={{ height: '100%', borderRight: 0 }}
+        >
+          <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+            <Menu.Item key="1">option1</Menu.Item>
+            <Menu.Item key="2">option2</Menu.Item>
+            <Menu.Item key="3">option3</Menu.Item>
+            <Menu.Item key="4">option4</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+            <Menu.Item key="5">option5</Menu.Item>
+            <Menu.Item key="6">option6</Menu.Item>
+            <Menu.Item key="7">option7</Menu.Item>
+            <Menu.Item key="8">option8</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
+            <Menu.Item key="9">option9</Menu.Item>
+            <Menu.Item key="10">option10</Menu.Item>
+            <Menu.Item key="11">option11</Menu.Item>
+            <Menu.Item key="12">option12</Menu.Item>
+          </SubMenu>
+        </Menu>
+      </Sider>
+      <Layout style={{ padding: '0 24px 24px' }}>
+        <Content
+          className="site-layout-background"
+          style={{
+            padding: 24,
+            marginTop: 24,
+            minHeight: 280
+          }}
+        >
+          Dashboard
+        </Content>
+      </Layout>
+    </Layout>
+  );
+}
+
+function Login() {
+  return (
+    <Layout style={{ padding: '0 24px 24px' }}>
+      <Content
+        className="site-layout-background"
+        style={{
+          padding: 24,
+          marginTop: 24,
+          minHeight: 280,
+        }}
+      >
+        Login
+      </Content>
+    </Layout>
+  );
+}
